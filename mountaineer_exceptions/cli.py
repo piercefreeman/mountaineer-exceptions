08fec25b25b7c4e865405d743224144e9924ba19
Pierce Freeman <piercefreeman@gmail.com>
2025-05-14 09:30:36 -0700

    Add postcss support to build

--- mountaineer_exceptions/cli.py
+++ mountaineer_exceptions/cli.py
@@ -3,7 +3,7 @@ from mountaineer.cli import handle_build
 from mountaineer_exceptions.plugin import plugin
 from mountaineer_exceptions.views import get_core_view_path
 
-app = plugin.to_webserver(get_core_view_path(""))
+app = plugin.to_webserver()
 
 
 def build():
--- mountaineer_exceptions/plugin.py
+++ mountaineer_exceptions/plugin.py
@@ -1,11 +1,18 @@
-from mountaineer.plugin import MountaineerPlugin
+from mountaineer.plugin import MountaineerPlugin, BuildConfig
 
 from mountaineer_exceptions.controllers.exception_controller import ExceptionController
 from mountaineer_exceptions.views import get_core_view_path
+from mountaineer.client_compiler.postcss import PostCSSBundler
 
 plugin = MountaineerPlugin(
     name="mountaineer-exceptions",
     controllers=[ExceptionController],
     ssr_root=get_core_view_path("_ssr"),
     static_root=get_core_view_path("_static"),
+    build_config=BuildConfig(
+        view_root=get_core_view_path(""),
+        custom_builders=[
+            PostCSSBundler()
+        ]
+    ),
 )
